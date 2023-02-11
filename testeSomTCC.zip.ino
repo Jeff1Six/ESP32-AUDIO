4c61d6174425cd744d3be737b437bc053b181342
Jeff1Six <72455417+Jeff1Six@users.noreply.github.com>
2023-02-11 13:42:26 -0300

    Atualizacao para bateria fraca

--- testeSomTCC.zip.ino
+++ testeSomTCC.zip.ino
@@ -21,8 +21,8 @@
 Audio audio;
 Pangodream_18650_CL BL;
 WiFiMulti wifiMulti;
-String ssid =     "Jefferson/Jeniffer";
-String password = "78341675";
+String ssid =     "!!!!!!!!";
+String password = "!!!!!!!!";
 
 // Manipular task
 TaskHandle_t hTask1;
@@ -58,7 +58,7 @@ void loop() {
 void reproduzAudio() {
   HTTPClient http;
   http.addHeader("Content-Type", "application/json");
-  http.begin("http://192.168.0.105:8000/pessoas");
+  http.begin("http://192.168.0.103:8000/pessoas");
   int httpResponseCode = http.GET();
   if (httpResponseCode == 200) {
     Serial.print("HTTP Response Code: ");
@@ -82,22 +82,12 @@ void verificaBateria() {
   int valor_bateria = BL.getBatteryChargeLevel();
   HTTPClient http;
   http.addHeader("Content-Type", "application/json");
-  http.begin("http://192.168.0.105:8000/bateriaStatus?bateria=" + String(valor_bateria));
+  http.begin("http://192.168.0.103:8000/bateriaStatus?bateria=" + String(valor_bateria));
   int httpResponseCode = http.GET();
   if (httpResponseCode == 200) {
     Serial.print("HTTP Response Code: ");
     Serial.println(httpResponseCode);
     som = http.getString();
-    if (atoi(som.c_str()) != 100) {
-      if (!audio.isRunning()) {
-        audio.connecttospeech(som.c_str(), "pt-BR");
-      }
-      while (audio.isRunning())
-      {
-        audio.loop();
-      }
-    }
-
   } else {
     Serial.print("HTTP Response Code: ");
     Serial.println(httpResponseCode);
